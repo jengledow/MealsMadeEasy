270b22b5c90fb0bba39c6828d9e2566925ba0f15
jengledow <jmengledow5@gmail.com>
2025-08-10 21:51:34 -0400

    init

---
+++ mme/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+  import Recipe from './components/Recipe.vue';
+</script>
+
+<template>
+  <main>
+    <Recipe />
+  </main>
+</template>
---
+++ mme/src/components/Recipe.vue
@@ -0,0 +1,40 @@
+<script setup>
+	import { ref } from 'vue';
+	const title = ref("");
+	const ingredients = ref([]);
+	const newIngredient = ref("");
+
+	function addIngredient(){
+		ingredients.value.push(newIngredient.value);
+	}
+
+	function checkEnter(e){
+		if(e.key == "Enter"){
+			addIngredient();
+		}
+	}
+
+	function save(){
+		let recipe = {
+			title: title.value,
+			ingredients: [...ingredients.value] 
+		}
+
+		console.log(recipe);
+	}
+</script>
+
+<template>
+	<label for="title">
+		Name of Recipe:
+		<input type="text" placeholder="Recipe Title" v-model="title" id="title" />
+	</label>
+
+	<h2>{{ title }}</h2>
+	<ul v-for="i in ingredients">
+		<li>{{ i }}</li>
+	</ul>
+	<input v-model="newIngredient" type="text" placeholder="New Ingredient" @keyup="checkEnter" />
+	<button @click="addIngredient">Add</button>
+	<button @click="save">Save</button>
+</template>
